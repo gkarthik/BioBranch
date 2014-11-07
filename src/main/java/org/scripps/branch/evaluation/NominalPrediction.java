7c690295e9f4e222c038912da1bbc6c4b3a156d4
Karthik G <xraydiffraction@gmail.com>
2014-11-07 15:14:50 -0800

    Added AUC statistic.

---
+++ src/main/java/org/scripps/branch/evaluation/NominalPrediction.java
@@ -0,0 +1,40 @@
+package org.scripps.branch.evaluation;
+
+public class NominalPrediction extends weka.classifiers.evaluation.NominalPrediction{
+	  /** The predicted probabilities */
+	  private double [] m_Distribution;
+
+	  /** The actual class value */
+	  private double m_Actual = MISSING_VALUE;
+
+	  /** The predicted class value */
+	  private double m_Predicted = MISSING_VALUE;
+
+	  /** The weight assigned to this prediction */
+	  private double m_Weight = 1;
+	  
+	  private double m_pred = -1;
+	  
+	public NominalPrediction(double actual, double[] distribution) {
+		super(actual, distribution);	
+	}
+	
+	  public NominalPrediction(double actual, double [] distribution, 
+              double weight) {
+		  super(actual, distribution,weight);
+	  }
+	
+	  public void updatePredicted() {
+		  if(m_pred != -1){
+			  m_Predicted = m_pred;
+		  }
+	  }
+
+	public double getM_pred() {
+		return m_pred;
+	}
+
+	public void setM_pred(double m_pred) {
+		this.m_pred = m_pred;
+	}
+}
